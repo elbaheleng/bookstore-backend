81514c0c96d45ca16fee0eaee505f0798f0d8c41
elbaheleng <elbaheleng@gmail.com>
2025-05-27 15:53:44 +0530

    user logout, payment complete

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const books = require("../model/bookModel");
+const stripe = require('stripe')('sk_test_51RSxyzBByBIEVjlsnfjxRcAhe04Mkkjz2vtaNiMaoXqMU2kum8Aa9ysZNZBgXaCN5CymvnnULWchpSMf6B2V5GPq00I7u25pDe');
 //to add books
 exports.addBookController = async (req, res) => {
     //console.log('inside addBookController');
@@ -62,7 +63,7 @@ exports.getAllBookController = async (req, res) => {
 //to get a particular book
 exports.getABookController = async (req, res) => {
     const { id } = req.params
-   // console.log(id);
+    // console.log(id);
 
     try {
         const aBooks = await books.findOne({ _id: id })
@@ -74,10 +75,10 @@ exports.getABookController = async (req, res) => {
     }
 }
 //to get all books added by user
-exports.getAllUserAddedBooksController = async (req,res) => {
+exports.getAllUserAddedBooksController = async (req, res) => {
     const email = req.payload
     try {
-        const allBooksByUser = await books.find({userMail : email})
+        const allBooksByUser = await books.find({ userMail: email })
         res.status(200).json(allBooksByUser)
     } catch (error) {
         res.status(500).json(error)
@@ -85,10 +86,10 @@ exports.getAllUserAddedBooksController = async (req,res) => {
     }
 }
 //to get all books bought by user
-exports.getAllUserBoughtBookController = async (req,res) => {
+exports.getAllUserBoughtBookController = async (req, res) => {
     const email = req.payload
     try {
-        const allBooksBoughtByUser = await books.find({bought : email})
+        const allBooksBoughtByUser = await books.find({ bought: email })
         res.status(200).json(allBooksBoughtByUser)
     } catch (error) {
         res.status(500).json(error)
@@ -97,10 +98,10 @@ exports.getAllUserBoughtBookController = async (req,res) => {
 }
 
 
-exports.deleteAUserBookController = async (req,res) =>{
-    const {id} = req.params
-     try {
-        const deleteBook = await books.findByIdAndDelete({_id:id})
+exports.deleteAUserBookController = async (req, res) => {
+    const { id } = req.params
+    try {
+        const deleteBook = await books.findByIdAndDelete({ _id: id })
         res.status(200).json("Book deleted successfully")
     } catch (error) {
         res.status(500).json(error)
@@ -108,8 +109,76 @@ exports.deleteAUserBookController = async (req,res) =>{
     }
 }
 //payment controller
-exports.makePaymentController = async (req,res) =>{
+exports.makePaymentController = async (req, res) => {
+    const { booksDetails } = req.body
+    const email = req.payload
+    //console.log(booksDetails);
     
+    try {
+        const existingBook = await books.findByIdAndUpdate({ _id: booksDetails._id }, {
+            title: booksDetails.title,
+            author: booksDetails.author,
+            noofpages: booksDetails.noofpages,
+            imageurl: booksDetails.imageurl,
+            price: booksDetails.price,
+            dprice: booksDetails.dprice,
+            abstract: booksDetails.abstract,
+            publisher: booksDetails.publisher,
+            language: booksDetails.language,
+            isbn: booksDetails.isbn,
+            category: booksDetails.category,
+            uploadedImage: booksDetails.uploadedImage,
+            status: "sold",
+            userMail: booksDetails.userMail,
+            bought: email
+        }, { new: true })
+        console.log(existingBook);
+        
+        //create stripe checkout session
+        const line_item = [{
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: booksDetails.title,
+                    description: `${booksDetails.author} | ${booksDetails.abstract}`,
+                    images: [booksDetails.imageurl],
+                    metadata: {
+                        title: booksDetails.title,
+                        author: booksDetails.author,
+                        noofpages:` ${booksDetails.noofpages}`,
+                        imageurl: booksDetails.imageurl,
+                        price: `${booksDetails.price}`,
+                        dprice: `${booksDetails.dprice}`,
+                        abstract: booksDetails.abstract.slice(0,20),
+                        publisher: booksDetails.publisher,
+                        language: booksDetails.language,
+                        isbn: booksDetails.isbn,
+                        category: booksDetails.category,
+                       // uploadedImage: booksDetails.uploadedImage,
+                        status: "sold",
+                        userMail: booksDetails.userMail,
+                        bought: email
+                    }
+                },
+                unit_amount: Math.round(booksDetails.dprice*100) // multiplied by 100 to convert cents to dollar, math.round to round to nearest integer
+            },
+            quantity: 1
+        }]
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'], //purchased using card
+            line_items: line_item, // details of produt that is being purchased
+            mode: "payment",//make payment  or subscription or setup
+            success_url: 'http://localhost:5173/payment-success',//url to be shown if payment is successful
+            cancel_url: 'http://localhost:5173/payment-error'//url to be shown if payment is failed
+        });
+        console.log(session);
+        
+        res.status(200).json({sessionId:session.id })
+    
+    } catch (error) {
+        res.status(500).json(error)
+
+    }
 }
 //--------------------------------------------------------
 //------------admin--------------
@@ -130,7 +199,7 @@ exports.getAllBookAdminController = async (req, res) => {
 exports.approveBookController = async (req, res) => {
     const { _id, title, author, noofpages, imageurl, price, dprice, abstract, publisher, language, isbn, category, uploadedImage, status, userMail, bought } = req.body
     console.log(_id, title, author, noofpages, imageurl, price, dprice, abstract, publisher, language, isbn, category, uploadedImage, status, userMail, bought);
-    
+
     try {
         const existingBook = await books.findByIdAndUpdate({ _id }, { _id, title, author, noofpages, imageurl, price, dprice, abstract, publisher, language, isbn, category, uploadedImage, status: "approved", userMail, bought }, { new: true })
         //await existingBook.save() // here optional
